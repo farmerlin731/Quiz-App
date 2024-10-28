bea973f4c0c41fe0b752e8a8a018672580221ee8
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-10-28 20:13:34 +0800

    feat: 1.Shuffle the answers. / 2.Set the quiz complete img.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
+  // The following way for setting useState is not the optimal.
+  // const [activeQuestIndex,setActiveQuestIndex] = useState(0);
+  // const [userAns,setUserAns] = useState([]);
+
   const [ansArr, setAnsArr] = useState([]);
-  const questIndex = ansArr.length;
+  const questIndex = ansArr.length; //This means the order of questions is not random.
   function handleAnsClick(ans) {
     setAnsArr((prev) => [...prev, ans]);
   }
+
+  //Set the boundary of questions.
+  if (ansArr.length === QUESTIONS.length) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="A trophy!" />
+        <h2>Quiz Complete :)</h2>
+      </div>
+    );
+  }
+
+  //Shuffle Answers
+  const shufAns = [...QUESTIONS[questIndex].answers];
+  shufAns.sort((a, b) => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[questIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[questIndex].answers.map((ans) => (
+          {shufAns.map((ans) => (
             <li className="answer" key={ans}>
               <button onClick={() => handleAnsClick(ans)}>{ans}</button>
             </li>
